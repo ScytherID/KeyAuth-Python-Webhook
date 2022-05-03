84fbf85278f63ccd0df9fa6d0f214a4d9734c2bb
ScytherID <104829601+ScytherID@users.noreply.github.com>
2022-05-03 16:46:01 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from multiprocessing.sharedctypes import Value
+from turtle import color
+from unicodedata import name
+from urllib import response
+from keyauth import api
+from discord_webhook import DiscordWebhook , DiscordEmbed
+import os
+import sys
+import os.path
+import platform
+import hashlib
+from time import sleep
+from datetime import datetime
+
+print("Initializing")
+def getchecksum():
+    path = os.path.basename(__file__)
+    if not os.path.exists(path):
+    	path = path[:-2] + "exe"
+    md5_hash = hashlib.md5()
+    a_file = open(path,"rb")
+    content = a_file.read()
+    md5_hash.update(content)
+    digest = md5_hash.hexdigest()
+    return digest
+keyauthapp = api(
+	name = "",
+	ownerid = "",
+	secret = "",
+	version = "1.0",
+	hash_to_check = getchecksum()
+)
+sleep(1.5) # rate limit
+print(f"Login Status : {keyauthapp.check()}")
+sleep(1.5) 
+print(f"Checking Hwid Ban? : {keyauthapp.checkblacklist()}") 
+if keyauthapp.checkblacklist() == True:
+	print("You got hwid ban:( ")
+	print("So sad go use hwid spoofer xd")
+	contentss = "Warning Ban user try to login Tools!!" 
+	webhooks = DiscordWebhook(url="https://discordapp.com/api/webhooks/970699932946268281/zu3_K7ns0r_ZUIgn71HToofAEjCxgr5oUFscBpC4iC0VkcLAud4Cp_TUFUB1N5PMrs0h", username="Key info", content=contentss)
+	webhooks.execute()
+	sleep(1.5)
+	exit(0)
+
+sleep(1.5)
+print ("Do you want login (y/n) ?")
+ans=input("Choice please: ") 
+if ans=="y": 
+	user = input('Username: ')
+	password = input('Password: ')
+	keyauthapp.login(user,password)
+elif ans=="n":
+	print("Cencel Login!") 
+	sys.exit()
+
+usernameid = "Username : " + keyauthapp.user_data.username + "\n" + "Ip : " + keyauthapp.user_data.ip + "\n" + "Hwid : " + keyauthapp.user_data.hwid 
+print(f"Login Status: {keyauthapp.check()}")
+webhook = DiscordWebhook(url="Your Webhook", username="Name Webhook", content=usernameid)
+response = webhook.execute()
+print("Exit Tools...")
+sleep(10)
+exit(0)
